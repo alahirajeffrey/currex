49e1bb70f15d1d02e71d6d0f81bcb20265a30db6
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-19 20:27:12 +0200

    connected to mongofb

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ const app = express();
 
 app.use(express.json());
 
-// mongoose.connect('mongodb://localhost:27017/your-database-name', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(() => {
-//     console.log('Connected to MongoDB');
-// }).catch(err => {
-//     console.error('Could not connect to MongoDB', err);
-// });
+// connect to mongodb
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+  });
 
 export default app;
