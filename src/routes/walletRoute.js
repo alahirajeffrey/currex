5e1c7d04ab8bb91dff68a0fb0a6b12de7a136e42
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-20 16:40:26 +0200

    wallet routes

--- src/routes/walletRoute.js
+++ src/routes/walletRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
 import WalletController from "../controllers/walletController.js";
 import WalletService from "../services/walletService.js";
+import WalletRepository from "../repositories/walletRepository.js";
+import Wallet from "../models/Wallet.js";
 
-const walletService = new WalletService();
+const walletRepository = new WalletRepository(Wallet);
+const walletService = new WalletService(walletRepository);
 const walletController = new WalletController(walletService);
 
 const walletRouter = express.Router();
@@ -10,6 +13,12 @@ const walletRouter = express.Router();
 walletRouter.post(
   "/create",
   walletController.createWallet.bind(walletController)
+);
+
+// include validation
+walletRouter.post(
+  "/verify",
+  walletController.verifyWallet.bind(walletController)
 );
 
 export default walletRouter;
