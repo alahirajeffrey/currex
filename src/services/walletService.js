33b8638af6b835e3f9f34c9021798a66f739e69b
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-20 16:40:15 +0200

    wallet services

--- src/services/walletService.js
+++ src/services/walletService.js
@@ -1,9 +1,44 @@
 import bip39 from "bip39";
+import WalletRepository from "../repositories/walletRepository.js";
+import Wallet from "../models/Wallet.js";
 
 export default class WalletService {
-  constructor() {}
+  walletRepository = new WalletRepository(Wallet);
 
+  constructor(walletRepository) {
+    this.walletRepository = walletRepository;
+  }
+
+  /**
+   * generate 12 letter seed phrase
+   * @returns
+   */
   async generateSeedPhrase() {
     return bip39.generateMnemonic();
+  }
+
+  /**
+   * verify generated seed phrase
+   * @returns
+   */
+  async verifySeedPhrase(seedPhrase) {
+    return bip39.validateMnemonic(seedPhrase);
+  }
+
+  /**
+   * create wallet
+   * @returns
+   */
+  async createWallet() {
+    return await this.walletRepository.createWallet();
+  }
+
+  /**
+   * find wallet by id
+   * @param {*String} id: wallet id
+   * @returns
+   */
+  async findWalletById(id) {
+    return await this.walletRepository.findWalletById(id);
   }
 }
