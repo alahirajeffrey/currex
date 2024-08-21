1bf75f9ce6093e13041b89fc5eb48064ce66c247
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-21 11:53:34 +0200

    updated wallet model

--- src/repositories/walletRepository.js
+++ src/repositories/walletRepository.js
@@ -1,13 +1,23 @@
 import Wallet from "../models/Wallet.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV;
 
 class WalletRepository {
   constructor(Wallet) {
     this.walletModel = Wallet;
   }
 
-  async createWallet() {
+  async createWallet(publicKey) {
     try {
-      return await this.walletModel.create({});
+      const balance = NODE_ENV === "development" ? 5000 : 0;
+
+      return await this.walletModel.create({
+        publicKey: publicKey,
+        balance: balance,
+      });
     } catch (error) {
       throw error;
     }
