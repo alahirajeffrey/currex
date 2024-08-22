1595f9d3b5eacbf4025174649b0eb949fbbe8e13
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-22 17:34:45 +0200

    aded pfidid field to rating model

--- backend/src/models/Rating.js
+++ backend/src/models/Rating.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const RatingSchema = new mongoose.Schema(
   {
     pfi: { type: String, required: true },
+    pfiDid: { type: String, required: true },
     walletId: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: false },
