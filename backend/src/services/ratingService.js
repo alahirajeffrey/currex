1cd208657199d08963475862992da9054ed91d4e
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-22 17:36:39 +0200

    service to get pfi rating

--- backend/src/services/ratingService.js
+++ backend/src/services/ratingService.js
@@ -1,5 +1,6 @@
 import RatingRepository from "../repositories/ratingRepository.js";
 import Rating from "../models/Rating.js";
+import allowedPfis from "../../../allowedPfis.json" assert { type: "json" };
 
 export default class RatingService {
   ratingRepository = new RatingRepository(Rating);
@@ -14,9 +15,10 @@ export default class RatingService {
    * @param {*String} walletId : id of wallet
    * @param {*Number} rating : rating from user
    * @param {*String} comment : comment from user
+   * @param {*String} pfiDid : did of the pfi
    * @returns : rating obkect
    */
-  async addRating(pfi, walletId, rating, comment) {
+  async addRating(pfi, walletId, rating, comment, pfiDid) {
     if (Number(rating) > 5) throw new Error("rating cannot be greater than 5");
     if (Number(rating) < 1) throw new Error("rating cannot be less than 1");
 
@@ -24,7 +26,8 @@ export default class RatingService {
       pfi,
       walletId,
       rating,
-      comment
+      comment,
+      pfiDid
     );
   }
 
@@ -37,7 +40,6 @@ export default class RatingService {
     const ratingExists = await this.ratingRepository.getSingleRatingAndComments(
       id
     );
-    console.log(ratingExists);
     if (!ratingExists) throw new Error("rating does not exist");
     return ratingExists;
   }
@@ -48,5 +50,27 @@ export default class RatingService {
    */
   async getAllRatingAndComments() {
     return await this.ratingRepository.getAllRatingAndComments();
+  }
+  /**
+   * get a pfi rating
+   * @param {*String} pfiDid: DID of pfi to search
+   * @returns pfi information and rating
+   */
+  async getPfiRating(pfiDid) {
+    // loop through given PFIs json file and get PFI details
+    const pfiDetails = allowedPfis.find((pfi) => pfi.did === pfiDid);
+    const ratings = await this.ratingRepository.getAllPfiRating(pfiDid);
+
+    let totalRating = 0;
+    for (const rating of ratings) {
+      totalRating += rating.rating;
+    }
+
+    const averageRating = totalRating / ratings.length;
+
+    return {
+      averageRating: averageRating,
+      pfiName: pfiDetails.name,
+    };
   }
 }
