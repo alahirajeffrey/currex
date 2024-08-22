3d5e02d9e01b9f8090aba1ff97f2af183db67dbc
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-22 10:06:04 +0200

    added currencies

--- backend/src/models/Wallet.js
+++ backend/src/models/Wallet.js
@@ -3,7 +3,17 @@ import mongoose from "mongoose";
 const walletSchema = new mongoose.Schema(
   {
     publicKey: { type: String, required: true },
-    balance: { type: Number, default: 0 },
+    GHS: { type: Number, default: 0 },
+    USDC: { type: Number, default: 0 },
+    NGN: { type: Number, default: 0 },
+    KES: { type: Number, default: 0 },
+    USD: { type: Number, default: 0 },
+    EUR: { type: Number, default: 0 },
+    GBP: { type: Number, default: 0 },
+    BTC: { type: Number, default: 0 },
+    BD: { type: Number, default: 0 },
+    AUD: { type: Number, default: 0 },
+    MXN: { type: Number, default: 0 },
   },
   { timestamps: true }
 );
