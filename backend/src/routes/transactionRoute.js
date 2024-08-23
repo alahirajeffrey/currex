4192ad9111595bf1a94e7cca3b6505d4ac3b7cf4
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-23 21:45:48 +0200

    added route to get offerings

--- backend/src/routes/transactionRoute.js
+++ backend/src/routes/transactionRoute.js
@@ -1,5 +1,18 @@
 import express from "express";
+import Transaction from "../models/Transaction.js";
+import TransactionService from "../services/transactionService.js";
+import TransactionController from "../controllers/transactionController.js";
+import TransactionRepository from "../repositories/transactionRepository.js";
+
+const transactionRepository = new TransactionRepository(Transaction);
+const transactionService = new TransactionService(transactionRepository);
+const tranasctionController = new TransactionController(transactionService);
 
 const transctionRouter = express.Router();
+
+transctionRouter.get(
+  "/offerings",
+  tranasctionController.getOfferings.bind(tranasctionController)
+);
 
 export default transctionRouter;
