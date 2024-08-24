58929b3238969a2e1e78923204bc635c04971565
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-24 10:23:14 +0100

    transaction repositories

--- backend/src/repositories/transactionRepository.js
+++ backend/src/repositories/transactionRepository.js
@@ -4,4 +4,30 @@ export default class TransactionRepository {
   constructor(Transaction) {
     this.transactionModel = Transaction;
   }
+
+  async createTransaction(walletId, from, to, pfi, pfiDid) {
+    try {
+      return await this.transactionModel.create({
+        walletId,
+        from,
+        to,
+        pfi,
+        pfiDid,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async attachIdToTransaction(transactionId) {
+    try {
+      const transaction = await this.transactionModel.findById(transactionId);
+      transaction.id = transaction._id;
+      transaction.save();
+
+      return transaction;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
