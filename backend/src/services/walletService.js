f147e4ce04b1f87d33cfb06b2c1b2ecc05d6e9c8
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-24 10:01:42 +0100

    service to create verifiable credential

--- backend/src/services/walletService.js
+++ backend/src/services/walletService.js
@@ -1,6 +1,7 @@
 import bip39 from "bip39";
 import WalletRepository from "../repositories/walletRepository.js";
 import Wallet from "../models/Wallet.js";
+import axios from "axios";
 
 export default class WalletService {
   walletRepository = new WalletRepository(Wallet);
@@ -40,5 +41,25 @@ export default class WalletService {
    */
   async findWalletById(id) {
     return await this.walletRepository.findWalletById(id);
+  }
+
+  /**
+   * create verifiable credential for customer
+   * @param {* String} customerName : Name of customer
+   * @param {* String} countryCode : Country code of customer
+   * @param {* String} customerDid : Customer DID
+   * @returns : axios response object
+   */
+  async createVerifiableCredetial(customerName, countryCode, customerDid) {
+    try {
+      const response = await axios.get(
+        `https://mock-idv.tbddev.org/kcc?name=${customerName}&country=${countryCode}&did=${customerDid}`
+      );
+
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
   }
 }
