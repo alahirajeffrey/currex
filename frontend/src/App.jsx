536455966dfe23bbaf4d51d181142badf95738ff
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-26 21:12:32 +0100

    setup app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" Component={Register} />
+        <Route path="/login" Component={Login} />
+        <Route />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
