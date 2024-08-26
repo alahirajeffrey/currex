e9ec69d5114987b446f2d1a3be18aae31f7d4451
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-26 21:22:01 +0100

    navbar component

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 text-white py-7">
+      <h1 className="text-center text-2xl font-bold">Currex Wallet</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
