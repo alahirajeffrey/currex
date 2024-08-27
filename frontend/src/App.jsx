c1aa24de2ae009330a652c912d02b4f8e0da418e
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-27 20:59:07 +0100

    imported dashbord

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/register" Component={Register} />
         <Route path="/login" Component={Login} />
+        <Route path="/dashboard" Component={Dashboard} />
         <Route />
       </Routes>
     </Router>
