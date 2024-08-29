70c2acf080b0df227ecfa0ad01686222d23ec2c2
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-29 16:22:02 +0100

    get pfi name from pfi did

--- frontend/src/components/Offerings.jsx
+++ frontend/src/components/Offerings.jsx
@@ -1,9 +1,11 @@
 import PropTypes from "prop-types";
+import allowedPfis from "../../../allowedPfis.json";
 
-const Offering = ({ pfiName, description, rate }) => {
+const Offering = ({ pfiDid, description, rate }) => {
+  const pfiData = allowedPfis.find((pfi) => pfi.did == pfiDid);
   return (
     <div className="p-4 mb-4 bg-gray-300 rounded-md">
-      <h3 className="text-lg font-bold text-black">{pfiName}</h3>
+      <h3 className="text-lg font-bold text-black">{pfiData.name}</h3>
       <p className="text-sm text-black">Description: {description}</p>
       <p className="text-sm text-black">Rate: {rate}</p>
     </div>
@@ -11,7 +13,7 @@ const Offering = ({ pfiName, description, rate }) => {
 };
 
 Offering.propTypes = {
-  pfiName: PropTypes.string.isRequired,
+  pfiDid: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   rate: PropTypes.string.isRequired,
 };
